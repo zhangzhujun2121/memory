1a155230fe6251fc2caedf8c5ae1cb4c0f4d4476
zhangzhujun2121 <80336416+zhangzhujun2121@users.noreply.github.com>
2021-05-18 18:24:43 +0800

    Add files via upload

---
+++ memory/memory/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace memory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "h3.ico");
+            this.imageList1.Images.SetKeyName(1, "h5.ico");
+            // 
+            // panel1
+            // 
+            this.panel1.ContextMenuStrip = this.contextMenuStrip1;
+            this.panel1.Location = new System.Drawing.Point(28, 16);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(296, 541);
+            this.panel1.TabIndex = 3;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.删除ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // 删除ToolStripMenuItem
+            // 
+            this.删除ToolStripMenuItem.Name = "删除ToolStripMenuItem";
+            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.删除ToolStripMenuItem.Text = "删除";
+            this.删除ToolStripMenuItem.Click += new System.EventHandler(this.删除ToolStripMenuItem_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(344, 393);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 31);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "分配";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(344, 330);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(342, 290);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "进程大小：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(344, 258);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(342, 238);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "进程名：";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(344, 184);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 31);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "选择颜色";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(344, 458);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 31);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "内存压缩";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 124);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 31);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "显示内存";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(341, 13);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "内存大小：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(343, 33);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 26);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "500";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(343, 70);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "页面大小：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(345, 90);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(132, 26);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.Text = "2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 573);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 删除ToolStripMenuItem;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ memory/memory/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace memory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int SystemMemory; //初始系统内存大小
+        int MemorySize;    //页面大小
+        List<area> l = new List<area>();    //内存链表
+        area dele = null;   //删除区域
+        public class area   //进程空闲区
+        {
+            public string ID;
+            public int beginAdress;
+            public int length;
+            public Color tag ;  //color 为白色就是空闲区
+            public int endAdress;
+
+            public area(string id,int begin, int len, Color t)
+            {
+                ID = id;
+                beginAdress = begin;
+                length = len;
+                tag = t;
+                endAdress = beginAdress + length;
+            }
+            public void set(int begin ,int len , Color t)
+            {
+                beginAdress = begin;
+                length = len;
+                tag = t;
+                endAdress = beginAdress + length;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void union()     //合并
+        {          
+            for (int i = 0; i < l.Count -1; i++)
+            {
+                if(l[i].tag==Color.White)
+                {
+                    if(l[i+1].tag==Color.White)
+                    {
+                        l[i].endAdress = l[i + 1].endAdress;
+                        l[i].length = l[i].length + l[i + 1].length;
+                        l.RemoveAt(i + 1);
+                    }
+                }
+            }
+            
+        }
+        private void insert()//插入
+        {
+            int len = Convert.ToInt32(textBox1.Text);
+            len = len % MemorySize == 0 ? len / MemorySize : len / MemorySize + 1;
+            foreach (var item in l)
+            {
+                if (item.tag == Color.White)
+                {
+                    if (item.length >= len)
+                    {
+                        l.Remove(item);
+
+                        area tmp = new area("", item.beginAdress, len, colorDialog1.Color);
+                        tmp.ID = textBox2.Text;
+                        item.set(tmp.endAdress, item.length - len, Color.White);
+                        item.ID = "空闲区";
+                        l.Add(tmp);
+                        if (item.length != 0)
+                        {
+                            l.Add(item);
+                        }
+                        return;
+                    }
+                }
+            }
+            MessageBox.Show("内存不足，请压缩内存或清理内存！");
+        }
+
+        private void delete()   //删除
+        {
+            foreach (var item in l)
+            {
+                if(item==dele)
+                {
+                    item.tag = Color.White;
+                    item.ID ="空闲区";
+                }
+            }
+        }
+
+        private void compress()     //内存压缩
+        {
+            List<area> tmp = new List<area>();
+
+            int shang = 0; //上一个非空尾地址
+            int len = 0;
+            foreach (var item in l)
+            {
+                if (item.tag == Color.White)
+                {
+                    len += item.length;
+                }
+                else
+                {
+                    item.beginAdress = shang;
+                    item.endAdress = shang + item.length;
+                    shang = item.endAdress;
+                    tmp.Add(item);
+                }
+            }
+            l = tmp;
+            if (len != 0)
+            {
+                area kong = new area("空闲区", shang, len, Color.White);
+                l.Add(kong);
+            }
+            paint();
+        }
+        private void paint()    //绘图
+        {
+            
+            Graphics g = panel1.CreateGraphics();
+            panel1.Controls.Clear();
+            g.Clear(BackColor);
+            panel1.Font = new Font("黑体", 9, panel1.Font.Style | FontStyle.Italic);
+            foreach (var item in l)
+            {
+
+                Brush brush = new SolidBrush(item.tag);             
+                g.FillRectangle(brush, 0, item.beginAdress * 2, 300, item.length * 2);
+                g.DrawLine(Pens.Black, 0, item.beginAdress * 2, 300, item.beginAdress * 2);
+                g.DrawString(item.beginAdress+"-"+(item.endAdress-1)+item.ID, Font, Brushes.Black, 100, item.beginAdress + item.endAdress);
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)  //插入按钮
+        {
+            
+
+            insert();
+            paint();
+        }
+
+        private void button3_Click(object sender, EventArgs e)  //选择颜色按钮
+        {
+
+            colorDialog1.AllowFullOpen = false;
+            //提供自己给定的颜色
+            colorDialog1.CustomColors = new int[] { 6916092, 15195440, 16107657, 1836924, 3758726, 12566463, 7526079, 7405793, 6945974, 241502, 2296476, 5130294, 3102017, 7324121, 14993507, 11730944 };
+            colorDialog1.ShowHelp = true;
+            colorDialog1.ShowDialog();
+            button3.BackColor = colorDialog1.Color;
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)  //选择区域
+        {
+            foreach (var item in l)
+            {
+                if(e.X<=300&&e.X>=0&&e.Y<=item.endAdress*2&&e.Y>=item.beginAdress*2)
+                {
+                    dele = item;
+                }
+            }
+        }
+
+        private void 删除ToolStripMenuItem_Click(object sender, EventArgs e) //右键删除
+        {
+            if(dele!=null)
+            {
+                delete();
+                union();
+                union();
+                paint();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)  //内存压缩按钮
+        {
+            compress();
+        }
+
+        private void button1_Click(object sender, EventArgs e)  //显示内存按钮
+        {
+            
+            l.Clear();
+            Graphics g = panel1.CreateGraphics();
+            SystemMemory = int.Parse(textBox3.Text.ToString());
+            MemorySize = int.Parse(textBox4.Text.ToString());
+            SystemMemory /= MemorySize;
+            area tmp = new area("空闲区", 0, SystemMemory, Color.White);
+            l.Add(tmp);
+            paint();
+        }
+    }
+}
